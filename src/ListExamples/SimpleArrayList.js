0edf17b9d623897509be1af3c6eaec5ce9cc1178
Elaman Imashov <elaman@outlook.com>
2020-04-06 11:03:11 +0600

    Filter

--- src/ListExamples/SimpleArrayList.js
+++ src/ListExamples/SimpleArrayList.js
@@ -3,7 +3,7 @@ import Fruit from "./Fruit";
 
 export default () => {
   const vegetables = ["Potatoes", "Tomatoes", "Onions"];
-  const [fruits, setFruits] = useState(["Apple", "Pear", "Banana", "Melon"]);
+  const [fruits, setFruits] = useState(["Pear", "Banana", "Melon", "Apple", "Ananas"]);
 
   // How to use .join to output array
   const vegetablesOutput = vegetables.join(", ");
@@ -16,6 +16,20 @@ export default () => {
     <Fruit key={fruit} name={fruit} />
   ));
 
+  // How to filter the array.
+  const fruitsA = fruits.filter(fruit => fruit[0] === "A");
+  const fruitsAOutput = fruitsA.map((fruit, index) => (
+    <li key={fruit} className={fruit + "-" + index}>{fruit}</li>
+  ));
+  // How to sort the array.
+  const fruitsSorted = fruits.sort();
+  const fruitsSortedOutput = fruitsSorted.map((fruit, index) => (
+    <li key={fruit} className={fruit + "-" + index}>{fruit}</li>
+  ));
+
+  // 4. [10, 100, 200, 512, 251, 155, 20, 39, 300, 401, 312, 560, 1000]
+  // Вывести числа между 100 и 500 в увеличивающемся порядке как HTML список
+
   return (
     <div className="SimpleArrayList">
       {vegetablesOutput}
@@ -23,6 +37,10 @@ export default () => {
       <ul>{fruitsOutput}</ul>
       <br />
       <ul>{pricedFruitsOutput}</ul>
+      <br />
+      <ul>{fruitsAOutput}</ul>
+      <br />
+      <ul>{fruitsSortedOutput}</ul>
     </div>
   );
 };
